6fbc3d47d92ff5bcc83d9b858f3616108e200922
paul <pdmxdd@gmail.com>
2020-07-16 22:46:08 -0500

    adding studio files

---
+++ launchcode-repos.ps1
@@ -0,0 +1,25 @@
+# after each prompt add the powershell command(s) you used to answer the question
+
+
+# Invoke-RestMethod -URI https://api.github.com/orgs/launchcodeeducation/repos
+
+# how many repositories are returned when making a GET request to ``https://api.github.com/orgs/launchcodeeducation/repos``?
+
+# what are the names of the repositories returned by a GET request to ``https://api.github.com/orgs/launchcodeeducation/repos``?
+
+# save all the names of the repositories in alphabetical order as a CSV file named ``launchcode-education-repo-names.csv``
+
+# are there any issues attached to the first repository in the list?
+
+# what is the issues_url for the first repository in the list?
+
+# make a new request to that issues_url
+
+# how many issues are found
+
+# how would you access the 5th issues of this list
+
+# what is the id, title, and URL for the fifth issues in this list?
+
+
+# upon completing this file push it to your GitHub repository # push the launchcode-education-repo-names.csv file as well
